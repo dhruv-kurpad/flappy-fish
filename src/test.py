8d6e3349c1166468957075e398709890ba69bf88
Kyle <kcbartel@wisc.edu>
2026-03-16 20:10:06 -0500

    adding python test file to project

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestFrontend(unittest.TestCase):
+
+    def test_pipe(self):
+        """Test the numpy mean function."""
+        self.assertEqual(true, true)
+
+if __name__ == '__main__':
+    unittest.main()
